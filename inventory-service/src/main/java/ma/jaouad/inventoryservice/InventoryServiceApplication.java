4cb4ca3af33d21b76774e318fa0206acc44bb863
jaouad4 <salahjd16@gmail.com>
2025-10-20 16:59:36 +0100

    Fix Bugs

--- inventory-service/src/main/java/ma/jaouad/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/ma/jaouad/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,34 @@
 package ma.jaouad.inventoryservice;
 
+import ma.jaouad.inventoryservice.entities.Product;
+import ma.jaouad.inventoryservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository) {
+        return args -> {
+            productRepository.saveAll(List.of(
+                    Product.builder().name("Ordinateur").price(5000.0).quantity(10).build(),
+                    Product.builder().name("Imprimante").price(1200.0).quantity(5).build(),
+                    Product.builder().name("Smartphone").price(3500.0).quantity(20).build(),
+                    Product.builder().name("Clavier").price(150.0).quantity(50).build()
+            ));
+            productRepository.findAll().forEach(System.out::println);
+        };
     }
 
 }
--- inventory-service/src/main/java/ma/jaouad/inventoryservice/entities/Product.java
+++ inventory-service/src/main/java/ma/jaouad/inventoryservice/entities/Product.java
@@ -2,6 +2,7 @@ package ma.jaouad.inventoryservice.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
