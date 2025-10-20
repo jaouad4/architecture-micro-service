22d4be31f0717adf7e639442197a6ca828792863
jaouad4 <salahjd16@gmail.com>
2025-10-20 16:13:55 +0100

    Add Inventory Micro Service

---
+++ inventory-service/src/main/java/ma/jaouad/inventoryservice/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package ma.jaouad.inventoryservice.repositories;
+
+import ma.jaouad.inventoryservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
